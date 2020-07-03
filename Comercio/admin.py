fce2fc34a79a97c47cf3f529b6380f29d3016ad9
facu <facu63829@gmail.com>
2020-07-03 17:03:31 -0300

    Trabajo casi finalizado, falta arreglar un poco Django Suits

--- Comercio/admin.py
+++ Comercio/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['rut', 'nombre','telefono',]
+    search_fields = ['nombre',]
+
+class ProductoInline(admin.TabularInline):
+    model = Producto
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['rut','nombre','telefono','web']
+    list_filter = ['rut','nombre']
+    inlines = [ProductoInline,]
+
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('id','cliente','descuento')
+    actions = ['cambiar_descuento',]
+
+    def cambiar_descuento (self, request, queryset):
+        if Venta.descuento == False:
+            return queryset.update(descuento=True)
+        #return queryset.update(descuento=True)
+
+#class ProductoAdmin(admin.ModelAdmin):
+
+
+admin.site.register(Direccion)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Detalle)
+admin.site.register(Cliente)
+admin.site.register(Venta)
--- Comercio/models.py
+++ Comercio/models.py
@@ -2,6 +2,57 @@ from django.db import models
 
 # Create your models here.
 
-class Direccion(models.Model):
+class Direccion (models.Model):
     calle = models.CharField(max_length = 30)
     ciudad = models.CharField(max_length = 30)
+    numero = models.CharField(max_length = 30)
+    def __str__(self):
+        return '{}'.format(self.calle)
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length = 30)
+    descripcion = models.CharField(max_length = 50)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+        
+class Proveedor (models.Model):
+    nombre = models.CharField(max_length = 20)
+    rut = models.IntegerField()
+    telefono = models.CharField(max_length = 50)
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    web = models.CharField(max_length=40)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=35)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    provedoor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    precio = models.CharField(max_length=15)
+    stock = models.CharField(max_length=15)
+    def __str__(self):
+        return self.nombre
+
+class Detalle (models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad=models.CharField(max_length=15)
+    def __str__(self):
+        return '{}'.format(self.producto)
+    
+class Cliente(models.Model):
+    rut = models.IntegerField()
+    nombre = models.CharField(max_length = 20)
+    telefono = models.CharField(max_length = 25)
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Venta(models.Model):
+    fecha = models.DateField()
+    detalle = models.ManyToManyField(Detalle)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    monto = models.CharField(max_length = 20)
+    def __str__(self):
+        return '{}'.format(self.monto)
+
+    
